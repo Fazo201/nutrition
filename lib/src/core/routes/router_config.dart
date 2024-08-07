68de875f4c1b11d90c8ed200ad35caf852eaa501
muminovabdulloh <muminovabdullokh@gmail.com>
2024-08-07 21:40:35 +0500

    Abdulloh Muminov, 6-commit, ReviewPage.

--- lib/src/core/routes/router_config.dart
+++ lib/src/core/routes/router_config.dart
@@ -1,13 +1,13 @@
-import 'package:flutter/cupertino.dart';
-import 'package:go_router/go_router.dart';
-import 'package:nutrition/src/core/routes/app_route_names.dart';
-import 'package:nutrition/src/feature/auth/view/pages/register_page.dart';
-import 'package:nutrition/src/feature/auth/view/pages/splash_page.dart';
+import "package:flutter/cupertino.dart";
+import "package:go_router/go_router.dart";
+import "package:nutrition/src/core/routes/app_route_names.dart";
+import "package:nutrition/src/feature/auth/view/pages/register_page.dart";
+import "package:nutrition/src/feature/auth/view/pages/splash_page.dart";
 import "package:nutrition/src/feature/main/view/pages/home_page.dart";
 
 GlobalKey<NavigatorState> parentNavigatorKey = GlobalKey<NavigatorState>();
 
-class RouterConfigService {
+mixin RouterConfigService {
   static final GoRouter router = GoRouter(
     initialLocation: AppRouteNames.homePage,
     debugLogDiagnostics: true,
@@ -15,23 +15,17 @@ class RouterConfigService {
       GoRoute(
         name: "SplashPage",
         path: AppRouteNames.splash,
-        builder: (BuildContext context, GoRouterState state) {
-          return const SplashPage();
-        },
+        builder: (BuildContext context, GoRouterState state) => const SplashPage(),
       ),
       GoRoute(
         name: "RegisterPage",
         path: AppRouteNames.register,
-        builder: (BuildContext context, GoRouterState state) {
-          return const RegisterPage();
-        },
+        builder: (BuildContext context, GoRouterState state) => const RegisterPage(),
       ),
       GoRoute(
         name: "HomePage",
         path: AppRouteNames.homePage,
-        builder: (BuildContext context, GoRouterState state) {
-          return  HomePage();
-        },
+        builder: (BuildContext context, GoRouterState state) => HomePage(),
       ),
     ],
   );
--- lib/src/core/widgets/app_material_context.dart
+++ lib/src/core/widgets/app_material_context.dart
@@ -1,10 +1,9 @@
-import 'package:flutter/material.dart';
-import "package:nutrition/src/feature/bookmark/view/pages/review_page.dart";
-
-import '../../feature/settings/inherited_locale_notifier.dart';
-import '../../feature/settings/inherited_theme_notifier.dart';
-import '../../feature/settings/locale_controller.dart';
-import '../../feature/settings/theme_controller.dart';
+import "package:flutter/material.dart";
+import "package:nutrition/src/feature/bookmark/view/pages/reviews_page.dart";
+import "../../feature/settings/inherited_locale_notifier.dart";
+import "../../feature/settings/inherited_theme_notifier.dart";
+import "../../feature/settings/locale_controller.dart";
+import "../../feature/settings/theme_controller.dart";
 import '../routes/router_config.dart';
 
 final ThemeController themeController = ThemeController();
