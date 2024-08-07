f589f4e842ee7d7eaaf3d1a92f2ea961ba1dc2d6
Boburbek6010 <boburbek@s.amity.edu>
2024-08-07 18:12:17 +0500

    Boburbek 7-commit, linter rules are added

--- lib/src/feature/auth/view/pages/register_page.dart
+++ lib/src/feature/auth/view/pages/register_page.dart
@@ -17,107 +17,106 @@ class RegisterPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) => Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-          child: Form(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Create an account',
-                  style: const AppTextStyle().registerCreateAccount,
-                ),
-                5.verticalSpace,
-                Text(
-                  'Let’s help you set up your account,\nit won’t take long.',
-                  style: const AppTextStyle().registerLetsHelp,
-                ),
-                20.verticalSpace,
-                const RegisterTextWidget(text: 'Name'),
-                5.verticalSpace,
-                const LoginTextfieldWidget(
-                  hintText: 'Enter Name',
-                ),
-                20.verticalSpace,
-                const RegisterTextWidget(text: 'Email'),
-                const SizedBox(height: 5),
-                const LoginTextfieldWidget(
-                  hintText: 'Enter Email',
-                ),
-                const SizedBox(height: 20),
-                const RegisterTextWidget(text: 'Password'),
-                const SizedBox(height: 5),
-                const LoginTextfieldWidget(
-                  hintText: 'Enter Password',
-                ),
-                const SizedBox(height: 20),
-                const RegisterTextWidget(text: 'Confirm Password'),
-                const SizedBox(height: 5),
-                const LoginTextfieldWidget(
-                  hintText: 'Retype Password',
-                ),
-                const SizedBox(height: 20),
-                Row(
-                  children: [
-                    Checkbox(
-                      checkColor: AppColors.cFF9C00,
-                      value: ref.read(loginVM).isCheck,
-                      onChanged: (value) {},
-                    ),
-                    Text(
-                      'Accept terms & Condition',
-                      style: const AppTextStyle().loginForgotPassword,
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 26),
-                const ElevatedButtonWidget(text: 'Sign Up'),
-                const SizedBox(height: 14),
-                const LoginOrWidget(),
-                const SizedBox(height: 15),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    LoginSizedboxWidget(
-                      svgPicture: 'assets/icons/sign_in_google_icon.svg',
-                    ),
-                    SizedBox(width: 25),
-                    LoginSizedboxWidget(
-                      svgPicture: 'assets/icons/sign_in_facebook_icon.svg',
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Already a member?",
-                      style: const AppTextStyle().loginDont,
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => LoginPage(),
-                          ),
-                        );
-                      },
-                      child: Text(
-                        " Sign In",
-                        style: const AppTextStyle().loginMinSign,
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+            child: Form(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Create an account',
+                    style: const AppTextStyle().registerCreateAccount,
+                  ),
+                  5.verticalSpace,
+                  Text(
+                    'Let’s help you set up your account,\nit won’t take long.',
+                    style: const AppTextStyle().registerLetsHelp,
+                  ),
+                  20.verticalSpace,
+                  const RegisterTextWidget(text: 'Name'),
+                  5.verticalSpace,
+                  const LoginTextfieldWidget(
+                    hintText: 'Enter Name',
+                  ),
+                  20.verticalSpace,
+                  const RegisterTextWidget(text: 'Email'),
+                  const SizedBox(height: 5),
+                  const LoginTextfieldWidget(
+                    hintText: 'Enter Email',
+                  ),
+                  const SizedBox(height: 20),
+                  const RegisterTextWidget(text: 'Password'),
+                  const SizedBox(height: 5),
+                  const LoginTextfieldWidget(
+                    hintText: 'Enter Password',
+                  ),
+                  const SizedBox(height: 20),
+                  const RegisterTextWidget(text: 'Confirm Password'),
+                  const SizedBox(height: 5),
+                  const LoginTextfieldWidget(
+                    hintText: 'Retype Password',
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    children: [
+                      Checkbox(
+                        checkColor: AppColors.cFF9C00,
+                        value: ref.read(loginVM).isCheck,
+                        onChanged: (value) {},
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                      Text(
+                        'Accept terms & Condition',
+                        style: const AppTextStyle().loginForgotPassword,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 26),
+                  const ElevatedButtonWidget(text: 'Sign Up'),
+                  const SizedBox(height: 14),
+                  const LoginOrWidget(),
+                  const SizedBox(height: 15),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      LoginSizedboxWidget(
+                        svgPicture: 'assets/icons/sign_in_google_icon.svg',
+                      ),
+                      SizedBox(width: 25),
+                      LoginSizedboxWidget(
+                        svgPicture: 'assets/icons/sign_in_facebook_icon.svg',
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Already a member?",
+                        style: const AppTextStyle().loginDont,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => LoginPage(),
+                            ),
+                          );
+                        },
+                        child: Text(
+                          " Sign In",
+                          style: const AppTextStyle().loginMinSign,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
-      ),
-    );
-  }
+      );
 }
