9389587f3988f813f8a4906af34f2e9518af93a2
azimovich <developerflutter27@gmail.com>
2024-08-07 18:20:22 +0500

    Shukhrat commit 0.6 - commit for update

--- lib/src/feature/home/view/pages/home_page.dart
+++ lib/src/feature/home/view/pages/home_page.dart
@@ -18,55 +18,58 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: const PreferredSize(
-        preferredSize: Size.fromHeight(143),
+        preferredSize: Size.fromHeight(160),
         child: HomePageAppBar(),
       ),
       backgroundColor: AppColors.white,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // custom tabbar
-            SizedBox(
-              height: 51,
-              width: double.infinity,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: tabBarItems.length,
-                itemBuilder: (ctx, i) {
-                  return MaterialButton(
-                    height: 31,
-                    minWidth: 54,
-                    elevation: 0,
-                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 7),
-                    color: curretIndex == i ? AppColors.c129575 : AppColors.white,
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-                    onPressed: () async {
-                      curretIndex = i;
-                      setState(() {});
-                    },
-                    child: Text(
-                      tabBarItems[i],
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(
-                        fontSize: 11,
-                        fontWeight: FontWeight.w600,
-                        color: curretIndex == i ? AppColors.white : AppColors.c71B1A1,
-                      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // custom tabbar
+          SizedBox(
+            height: 51,
+            width: double.infinity,
+            child: ListView.builder(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              scrollDirection: Axis.horizontal,
+              itemCount: tabBarItems.length,
+              itemBuilder: (ctx, i) {
+                return MaterialButton(
+                  height: 31,
+                  minWidth: 54,
+                  elevation: 0,
+                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 7),
+                  color: curretIndex == i ? AppColors.c129575 : AppColors.white,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+                  onPressed: () async {
+                    curretIndex = i;
+                    setState(() {});
+                  },
+                  child: Text(
+                    tabBarItems[i],
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      fontSize: 11,
+                      fontWeight: FontWeight.w600,
+                      color: curretIndex == i ? AppColors.white : AppColors.c71B1A1,
                     ),
-                  );
-                },
-              ),
+                  ),
+                );
+              },
             ),
-            const SizedBox(height: 15),
+          ),
+          const SizedBox(height: 15),
 
-            // home page main cards
-            SizedBox(
+          // home page main cards
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 2),
+            child: SizedBox(
               height: 231,
               width: double.infinity,
               child: ListView.separated(
+                padding: const EdgeInsets.symmetric(horizontal: 28),
                 itemCount: 4,
                 scrollDirection: Axis.horizontal,
                 itemBuilder: (_, i) {
@@ -80,27 +83,33 @@ class _HomePageState extends State<HomePage> {
                 separatorBuilder: (BuildContext context, int index) => SizedBox(width: 15),
               ),
             ),
+          ),
 
-            const SizedBox(height: 20),
+          const SizedBox(height: 20),
 
-            // home page text
-            const Text(
+          // home page text
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: Text(
               'New Recipes',
               style: TextStyle(
                 fontSize: 16,
                 fontWeight: FontWeight.w600,
               ),
             ),
+          ),
 
-            const SizedBox(height: 5),
-            // home page bottom card
-            SizedBox(
+          const SizedBox(height: 5),
+          // home page bottom card
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 2),
+            child: SizedBox(
               height: 127,
               width: double.infinity,
               child: ListView.separated(
                 itemCount: 4,
                 scrollDirection: Axis.horizontal,
-                padding: EdgeInsets.symmetric(horizontal: 6),
+                padding: const EdgeInsets.symmetric(horizontal: 28),
                 itemBuilder: (_, i) {
                   return const HomePageBottomCardWidget(
                     imageUrl: 'https://www.freepnglogos.com/uploads/food-png/food-sutherland-foodservice-12.png',
@@ -112,9 +121,9 @@ class _HomePageState extends State<HomePage> {
                 },
                 separatorBuilder: (BuildContext context, int index) => SizedBox(width: 15),
               ),
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
--- lib/src/feature/home/view_model/home_page_vm.dart
+++ lib/src/feature/home/view_model/home_page_vm.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+
+
+
 class HomePageVm with ChangeNotifier {
+  int curretIndex = 0;
+  var tabBarItems = ['All', 'Indian', 'Italian', 'Asian', 'Chinese', 'Uzbekistan', 'USA', 'Turkey'];
   void navigateToSearchPage(BuildContext context) {}
 }
