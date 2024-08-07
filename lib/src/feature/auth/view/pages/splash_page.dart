cb4b209a4ffa8d29f6567bff83e603ebe7c453c3
asliddinmusulmanov <musulmanovasliddin@gmail.com>
2024-08-07 06:23:46 -0700

    Asliddin commit 1

--- lib/src/feature/auth/view/pages/splash_page.dart
+++ lib/src/feature/auth/view/pages/splash_page.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 import 'package:nutrition/src/core/style/app_colors.dart';
 import 'package:nutrition/src/core/style/text_style.dart';
 import 'package:nutrition/src/feature/auth/view/pages/login_page.dart';
@@ -35,105 +35,103 @@ class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
   // }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xff171A21),
-      body: Container(
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: Image.asset('assets/images/splash_backgraund_image.png').image,
-            fit: BoxFit.cover,
+  Widget build(BuildContext context) => Scaffold(
+        backgroundColor: const Color(0xff171A21),
+        body: Container(
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: Image.asset('assets/images/splash_backgraund_image.png').image,
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  FadeTransition(
+                    opacity: _animation,
+                    child: Image.asset(
+                      'assets/images/img.png',
+                      height: 80,
+                      width: 80,
+                    ),
+                  ),
+                  const SizedBox(height: 14),
+                  FadeTransition(
+                    opacity: _animation,
+                    child: Text(
+                      "100K+ Premium Recipe ",
+                      // style: const AppTextStyle().splashTopText,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: MediaQuery.of(context).size.width * 0.45),
+              PreferredSize(
+                preferredSize: const Size(4, 4),
+                child: FadeTransition(
+                  opacity: _animation,
+                  child: SlideTransition(
+                    position: _animationIcons,
+                    child: Column(
+                      children: [
+                        Text(
+                          "Get",
+                          style: const AppTextStyle().splashCenter,
+                        ),
+                        Text(
+                          "Cooking",
+                          style: const AppTextStyle().splashCenter,
+                        ),
+                        const SizedBox(height: 20),
+                        Text(
+                          "Simple way to find Tasty Recipe",
+                          style: const AppTextStyle().splashButtomText,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: MediaQuery.of(context).size.width * 0.2),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 66),
+                child: FadeTransition(
+                  opacity: _animation,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: AppColors.c129575,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      fixedSize: const Size(double.maxFinite, 60),
+                    ),
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => LoginPage(),
+                        ),
+                      );
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Start Cooking",
+                          style: const AppTextStyle().splashButton,
+                        ),
+                        const SizedBox(width: 20),
+                        const Icon(Icons.arrow_forward_outlined),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Column(
-              children: [
-                FadeTransition(
-                  opacity: _animation,
-                  child: Image.asset(
-                    'assets/images/img.png',
-                    height: 80,
-                    width: 80,
-                  ),
-                ),
-                const SizedBox(height: 14),
-                FadeTransition(
-                  opacity: _animation,
-                  child: Text(
-                    "100K+ Premium Recipe ",
-                    style: const AppTextStyle().splashTopText,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: MediaQuery.of(context).size.width * 0.45),
-            PreferredSize(
-              preferredSize: const Size(4, 4),
-              child: FadeTransition(
-                opacity: _animation,
-                child: SlideTransition(
-                  position: _animationIcons,
-                  child: Column(
-                    children: [
-                      Text(
-                        "Get",
-                        style: const AppTextStyle().splashCenter,
-                      ),
-                      Text(
-                        "Cooking",
-                        style: const AppTextStyle().splashCenter,
-                      ),
-                      const SizedBox(height: 20),
-                      Text(
-                        "Simple way to find Tasty Recipe",
-                        style: const AppTextStyle().splashButtomText,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: MediaQuery.of(context).size.width * 0.2),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 66),
-              child: FadeTransition(
-                opacity: _animation,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: AppColors.c129575,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    fixedSize: const Size(double.maxFinite, 60),
-                  ),
-                  onPressed: () {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => LoginPage(),
-                      ),
-                    );
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Start Cooking",
-                        style: const AppTextStyle().splashButton,
-                      ),
-                      const SizedBox(width: 20),
-                      const Icon(Icons.arrow_forward_outlined),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+      );
 }
