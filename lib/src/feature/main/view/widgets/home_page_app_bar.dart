4855bbb263adceb2e17ee5292e09d5f121758d88
azimovich <developerflutter27@gmail.com>
2024-08-07 19:22:03 +0500

    Shukhrat commit 0.7 - commit for update

--- lib/src/feature/main/view/widgets/home_page_app_bar.dart
+++ lib/src/feature/main/view/widgets/home_page_app_bar.dart
@@ -2,7 +2,8 @@ import "package:flutter/material.dart";
 import "package:flutter_screenutil/flutter_screenutil.dart";
 import "package:flutter_svg/flutter_svg.dart";
 import "package:nutrition/src/core/style/app_colors.dart";
-import "package:nutrition/src/feature/search/view/pages/search_recipes_page.dart";
+
+import "../pages/search_recipes_page.dart";
 
 class HomePageAppBar extends StatelessWidget {
   const HomePageAppBar({super.key});
