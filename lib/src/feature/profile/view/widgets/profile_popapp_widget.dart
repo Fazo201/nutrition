469376abf4cb9b35ee262978c5e6dc04199af3b8
Azimjon <eshonxojayevazimjon951@gmail.com>
2024-08-08 17:23:47 +0500

    Azimjon. 1.8 profile page updated

--- lib/src/feature/profile/view/widgets/profile_popapp_widget.dart
+++ lib/src/feature/profile/view/widgets/profile_popapp_widget.dart
@@ -4,22 +4,13 @@ class ProfilePopappWidget extends StatelessWidget {
   const ProfilePopappWidget({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return  PopupMenuButton(
+  Widget build(BuildContext context) => PopupMenuButton(
       color: Colors.white,
       itemBuilder: (context) => [
         PopupMenuItem(
           // onTap: () => controller.pickAndUploadImage(context),
           child: Text("Rasmni o'zgartirish"),
         ),
-        // if(controller.profileModel!.profileImageUrl != null)
-        //   PopupMenuItem(
-        //     onTap: () => controller.deleteProfilerImage(context),
-        //     child: const Text("Rasmni o'chirish"),
-        //   ),
-
-
       ],
     );
-  }
 }
