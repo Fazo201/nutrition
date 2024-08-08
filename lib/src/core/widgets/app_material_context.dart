dff2ec3e1b019cf3c04ad782160f9def2d0699b0
azimovich <developerflutter27@gmail.com>
2024-08-08 14:48:40 +0500

    Shukhrat commit 0.8 - home page update

--- lib/src/core/widgets/app_material_context.dart
+++ lib/src/core/widgets/app_material_context.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import "package:nutrition/src/feature/bookmark/view/pages/review_page.dart";
+import "package:nutrition/src/feature/main/view/pages/home_page.dart";
 
 import '../../feature/settings/inherited_locale_notifier.dart';
 import '../../feature/settings/inherited_theme_notifier.dart';
@@ -29,7 +30,7 @@ class AppMaterialContext extends StatelessWidget {
             debugShowCheckedModeBanner: false,
             theme: InheritedThemeNotifier.maybeOf(context)?.theme,
             locale: InheritedLocalNotifier.maybeOf(context)?.appLocal,
-            home:  ReviewPage(),
+            home:  HomePage(),
           ),
         ),
       ),
