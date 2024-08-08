00793698c8c58c431f251821249bc511da806c8b
Diyorbek4926 <131335654+Diyorbek4926@users.noreply.github.com>
2024-08-08 17:49:44 +0500

    Diyorbek: Commit 9

--- lib/src/core/routes/app_route_names.dart
+++ lib/src/core/routes/app_route_names.dart
@@ -1,9 +1,11 @@
 final class AppRouteNames{
   static const String splash = "/splash";
   static const String register = "/register";
+  static const String primary = "/primary";
+  static const String recipePage = "/recipe_page";
 
   static const String homePage = "/home_page";
   static const String bookmark = "/bookmark";
-  static const String recipePage = "/recipe_page";
+  static const String notification = "/notification";
   static const String profile = "/profile";
 }
--- lib/src/core/routes/router_config.dart
+++ lib/src/core/routes/router_config.dart
@@ -3,6 +3,8 @@ import "package:go_router/go_router.dart";
 import "package:nutrition/src/core/routes/app_route_names.dart";
 import "package:nutrition/src/feature/auth/view/pages/register_page.dart";
 import "package:nutrition/src/feature/auth/view/pages/splash_page.dart";
+import "package:nutrition/src/feature/bookmark/view/pages/bookmark_page.dart";
+import "package:nutrition/src/feature/main/view/pages/primary_page.dart";
 import "package:nutrition/src/feature/profile/view/pages/profile_page.dart";
 import "package:nutrition/src/feature/recipe/view/pages/recipe_page.dart";
 
@@ -12,9 +14,38 @@ GlobalKey<NavigatorState> parentNavigatorKey = GlobalKey<NavigatorState>();
 
 class RouterConfigService {
   static final GoRouter router = GoRouter(
-    initialLocation: AppRouteNames.recipePage,
+    initialLocation: AppRouteNames.primary,
     debugLogDiagnostics: true,
     routes: <RouteBase>[
+      ShellRoute(
+        builder: (context, state, child) => PrimaryPage(child),
+        routes: [
+          GoRoute(
+            name: "HomePage",
+            path: AppRouteNames.homePage,
+            builder: (BuildContext context, GoRouterState state) => HomePage(),
+          ),
+          GoRoute(
+            name: "Bookmark",
+            path: AppRouteNames.bookmark,
+            builder: (BuildContext context, GoRouterState state) => BookmarkPage(),
+          ),
+          // GoRoute(
+          //   name: "Notification",
+          //   path: AppRouteNames.notification,
+          //   builder: (BuildContext context, GoRouterState state) => Notification(),
+          // ),
+          //profile page
+          GoRoute(
+            name: "ProfilePage",
+            path: AppRouteNames.profile,
+            builder: (BuildContext context, GoRouterState state) {
+              return const ProfilePage();
+            },
+          ),
+        ]
+      ),
+
       //splash page
       GoRoute(
         name: "SplashPage",
@@ -30,20 +61,6 @@ class RouterConfigService {
         builder: (BuildContext context, GoRouterState state) {
           return const RegisterPage();
         },
-      ),
-      //profile page
-      GoRoute(
-        name: "ProfilePage",
-        path: AppRouteNames.profile,
-        builder: (BuildContext context, GoRouterState state) {
-          return const ProfilePage();
-        },
-      ),
-      // home page
-      GoRoute(
-        name: "HomePage",
-        path: AppRouteNames.homePage,
-        builder: (BuildContext context, GoRouterState state) => HomePage(),
       ),
       //recipe_page
       GoRoute(
