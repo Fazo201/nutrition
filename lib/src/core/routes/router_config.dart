f618c45af1cd61acfd5cdd9a4798c7b98484ccd9
Diyorbek4926 <131335654+Diyorbek4926@users.noreply.github.com>
2024-08-08 18:24:15 +0500

    Diyorbek: Commit 12

--- lib/src/core/routes/router_config.dart
+++ lib/src/core/routes/router_config.dart
@@ -1,13 +1,15 @@
-import 'package:flutter/cupertino.dart';
-import 'package:go_router/go_router.dart';
-import 'package:nutrition/src/core/routes/app_route_names.dart';
-import 'package:nutrition/src/feature/auth/view/pages/register_page.dart';
-import 'package:nutrition/src/feature/auth/view/pages/splash_page.dart';
+import "package:flutter/cupertino.dart";
+import "package:go_router/go_router.dart";
+import "package:nutrition/src/core/routes/app_route_names.dart";
+import "package:nutrition/src/feature/auth/view/pages/register_page.dart";
+import "package:nutrition/src/feature/auth/view/pages/splash_page.dart";
 
 import "../../feature/bookmark/view/pages/bookmark_page.dart";
 import "../../feature/main/view/pages/home_page.dart";
 import "../../feature/main/view/pages/primary_page.dart";
-import "../../feature/main/view/pages/search_recipes_page.dart";
+import "../../feature/notification/view/pages/notification_page.dart";
+import "../../feature/profile/view/pages/profile_page.dart";
+import "../../feature/recipe/view/pages/recipe_page.dart";
 
 GlobalKey<NavigatorState> parentNavigatorKey = GlobalKey<NavigatorState>();
 
