4da48a43c9e51100e641700804dbd1bb0ebc5789
Diyorbek4926 <131335654+Diyorbek4926@users.noreply.github.com>
2024-08-08 17:37:42 +0500

    Diyorbek: Commit 8

--- lib/src/feature/main/view/pages/primary_page.dart
+++ lib/src/feature/main/view/pages/primary_page.dart
@@ -1,6 +1,8 @@
 // import 'package:d_navigation_bar/d_navigation_bar.dart';
 import "package:flutter/material.dart";
 import "package:flutter_svg/flutter_svg.dart";
+import "package:go_router/go_router.dart";
+import "package:nutrition/src/core/routes/app_route_names.dart";
 
 class PrimaryPage extends StatelessWidget {
   final Widget child;
@@ -10,9 +12,15 @@ class PrimaryPage extends StatelessWidget {
   Widget build(BuildContext context) => Scaffold(
       body: child,
       bottomNavigationBar: BottomNavigationBar(
-        onTap: (value) {
-
-        },
+        onTap: (index) => context.go(
+          index == 0
+              ? AppRouteNames.homePage
+              : index == 1
+              ? AppRouteNames.bookmark
+              : index == 2
+              ? AppRouteNames.recipePage
+              : AppRouteNames.profile,
+        ),
         backgroundColor: Colors.white,
         showSelectedLabels: false,
         showUnselectedLabels: false,
