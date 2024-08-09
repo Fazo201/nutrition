09a30be19c110177d16571680fd7d391dbde7bde
Azimjon <eshonxojayevazimjon951@gmail.com>
2024-08-09 10:21:50 +0500

    Azimjon. 1.8 profile pageda rasm oqiydigan funksiya qoshildi

--- lib/src/feature/profile/view_model/profile_vm.dart
+++ lib/src/feature/profile/view_model/profile_vm.dart
@@ -3,6 +3,7 @@ import "package:file_picker/file_picker.dart";
 import "package:flutter/cupertino.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:path_provider/path_provider.dart";
+import "package:shared_preferences/shared_preferences.dart";
 import "../view/widgets/profile_delate_diolog_widget.dart";
 final profileVM = ChangeNotifierProvider((ref) => ProfileVm());
 
@@ -23,32 +24,54 @@ class ProfileVm extends ChangeNotifier {
       final File file = File(platformFile.path!);
       final directory = await getApplicationDocumentsDirectory();
       await file.copy("${directory.path}/image.png");
+      final String savedPath = "${directory.path}/image.png";
+      await file.copy(savedPath);
+
+      // Rasm pathini saqlash
+      await saveProfileImagePath(savedPath);
+
 
       if (context.mounted) {
         profileImagePath = file.path;
         notifyListeners();
       }
-
     }
-
-
-
   }
+
+
+
+
 
 
   Future<void> read() async {
+    final savedPath = await getSavedProfileImagePath();
 
-    final directory = await getApplicationDocumentsDirectory();
-    await for (var event in directory.list()) {
-      if (event.path.contains('image.png')) {
-        isImageSelected = true;
-      }
-    }
-    if(isImageSelected){
-      profileImagePath = File("${directory.path}/image.png") as String?;
-     notifyListeners();
+    if (savedPath != null && File(savedPath).existsSync()) {
+      isImageSelected = true;
+      profileImagePath = savedPath;
+      notifyListeners();
     }
   }
+
+
+
+// Profil rasmini pathini saqlash funktsiyasi
+  Future<void> saveProfileImagePath(String path) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('profile_image_path', path);
+  }
+
+// Saqlangan pathdan rasmni o'qib olish funktsiyasi
+  Future<String?> getSavedProfileImagePath() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString('profile_image_path');
+  }
+
+
+
+
+
+
 
   Future<void> deleteProfilerImage(BuildContext context) async {
     await Future.delayed(const Duration(milliseconds: 150));
