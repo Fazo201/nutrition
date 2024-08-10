4bebf376ac5c9fd0ade4e5f22a8e0fbadff5bef7
muminovabdulloh <muminovabdullokh@gmail.com>
2024-08-10 16:47:05 +0500

    Abdulloh Muminov, 17-commit, bug has fixed.

--- lib/src/core/constants/context_extension.dart
+++ lib/src/core/constants/context_extension.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
 extension CustomContextExtension on BuildContext {
   MediaQueryData get mediaQuery => MediaQuery.of(this);
