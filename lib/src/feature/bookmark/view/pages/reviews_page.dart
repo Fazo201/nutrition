d53b6470b3fb3b502043ce6a50b300f84bc18b44
muminovabdulloh <muminovabdullokh@gmail.com>
2024-08-16 17:12:49 +0500

    Abdulloh Muminov, 20-commit, Shimmer effect has added to reviews page.

--- lib/src/feature/bookmark/view/pages/reviews_page.dart
+++ lib/src/feature/bookmark/view/pages/reviews_page.dart
@@ -3,6 +3,8 @@ import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:flutter_screenutil/flutter_screenutil.dart";
 import "package:nutrition/src/core/constants/context_extension.dart";
 import "package:nutrition/src/feature/bookmark/vm/review_vm.dart";
+import "package:shimmer/shimmer.dart";
+
 import "../../../../core/style/app_colors.dart";
 import "../widgets/review_page_text_field_widget.dart";
 import "../widgets/review_send_button_widget.dart";
@@ -13,7 +15,8 @@ class ReviewsPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-     ref.watch(reviewVM);
+    final reviews = ref.watch(reviewVM.notifier).reviews;
+    final isLoading = reviews.isEmpty;
 
     return Scaffold(
       backgroundColor: AppColors.white,
@@ -45,7 +48,7 @@ class ReviewsPage extends ConsumerWidget {
                   ),
                 ),
                 Text(
-                  "${ref.watch(reviewVM).reviews.length} ${context.localized.comments}",
+                  "${reviews.length} ${context.localized.comments}",
                   style: context.theme.textTheme.labelSmall?.copyWith(
                     fontSize: 14.sp,
                     fontFamily: "Poppins",
@@ -64,7 +67,7 @@ class ReviewsPage extends ConsumerWidget {
                   ReviewSendButtonWidget(
                     text: context.localized.send,
                     onPressed: () {
-                      ref.watch(reviewVM).addReview();
+                      ref.read(reviewVM).addReview();
                       FocusScope.of(context).unfocus();
                     },
                   ),
@@ -72,8 +75,59 @@ class ReviewsPage extends ConsumerWidget {
               ),
             ),
             Expanded(
-              child: ListView.builder(
-                itemCount: ref.watch(reviewVM).reviews.length,
+              child: isLoading
+                  ? ListView.builder(
+                itemCount: 10, // Number of shimmer placeholders
+                itemBuilder: (_, index) => Shimmer.fromColors(
+                  baseColor: Colors.grey[300]!,
+                  highlightColor: Colors.grey[100]!,
+                  child: Container(
+                    margin: EdgeInsets.symmetric(vertical: 8.h),
+                    padding: EdgeInsets.all(16.w),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8.r),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.2),
+                          spreadRadius: 1,
+                          blurRadius: 5,
+                          offset: Offset(0, 2),
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 24.r,
+                          backgroundColor: Colors.grey,
+                        ),
+                        16.horizontalSpace,
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                color: Colors.grey,
+                                height: 12.h,
+                                width: 100.w,
+                              ),
+                              8.verticalSpace,
+                              Container(
+                                color: Colors.grey,
+                                height: 8.h,
+                                width: 150.w,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              )
+                  : ListView.builder(
+                itemCount: reviews.length,
                 itemBuilder: (_, index) => ReviewWidget(
                   reviewIndex: index,
                 ),
